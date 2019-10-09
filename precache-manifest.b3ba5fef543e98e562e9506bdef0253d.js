dcd0e4a00e76922d1c4e2da9fc4bbe9189920f8a
mmarsden89 <mmarsden89@gmail.com>
2019-10-09 13:38:56 -0400

    Updates

--- precache-manifest.b3ba5fef543e98e562e9506bdef0253d.js
+++ precache-manifest.b3ba5fef543e98e562e9506bdef0253d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "868e8674c34b11a5de7d36091dab1cd0",
+    "revision": "b83e7e2513ee2d4e3b82feea822f622b",
     "url": "/national-park-prototype/index.html"
   },
   {
-    "revision": "498f2a4661cbc3de2f89",
+    "revision": "5a9c8a3d0a4a093171fd",
     "url": "/national-park-prototype/static/css/main.4c28bec3.chunk.css"
   },
   {
-    "revision": "31e1d580712e3530d93d",
-    "url": "/national-park-prototype/static/js/2.ce0aab76.chunk.js"
+    "revision": "96b89f6d7f41fdab89b7",
+    "url": "/national-park-prototype/static/js/2.5e3b33f3.chunk.js"
   },
   {
-    "revision": "498f2a4661cbc3de2f89",
-    "url": "/national-park-prototype/static/js/main.90651b09.chunk.js"
+    "revision": "5a9c8a3d0a4a093171fd",
+    "url": "/national-park-prototype/static/js/main.b595bf7f.chunk.js"
   },
   {
     "revision": "39360f159a926d7f9709",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/national-park-prototype/precache-manifest.24b36eb47b3028494e8d93af3ab4faf5.js"
+  "/national-park-prototype/precache-manifest.b3ba5fef543e98e562e9506bdef0253d.js"
 );
 
 self.addEventListener('message', (event) => {
