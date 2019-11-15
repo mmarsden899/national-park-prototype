2965fd5cf22f3909c385d51814b349d73f1cba57
mmarsden89 <mmarsden89@gmail.com>
2019-11-15 15:47:59 -0500

    lets try this again

---
+++ src/SecuredRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import auth0Client from '../Auth';
+
+function SecuredRoute(props) {
+  const {component: Component, path, checkingSession} = props;
+  return (
+    <Route path={path} render={() => {
+      if (checkingSession) return <h3 className="text-center">Validating session...</h3>;
+      // ... leave the rest untouched ...
+     }} />
+  );
+}
+
+export default SecuredRoute;
