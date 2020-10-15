2920a9df37c60abcc64ccfcdad2d3d15989c8c8d
mmarsden89 <mmarsden89@gmail.com>
2020-10-14 19:52:49 -0700

    Add new auth0 implimentation for testing

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,35 +1,50 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom';
-import auth0Client from './Auth';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSignOutAlt, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { withRouter } from "react-router-dom";
+import auth0Client from "./Auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import Profile from "./Profile";
 
 function NavBar(props) {
-  console.log(props)
+  console.log(window.location.origin);
   const signOut = () => {
     auth0Client.signOut();
-    props.history.replace('/');
-  }
+    props.history.replace("/");
+  };
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {
-          !auth0Client.isAuthenticated() &&
-          <FontAwesomeIcon icon={faSignInAlt} className="signout" onClick={auth0Client.signIn}/>
-        }
-        {
-          auth0Client.isAuthenticated() &&
+        {!auth0Client.isAuthenticated() && (
+          <FontAwesomeIcon
+            icon={faSignInAlt}
+            className="signout"
+            onClick={auth0Client.signIn}
+          />
+        )}
+        {auth0Client.isAuthenticated() && (
           <div className="profile-area">
-            <label className="nickname">{auth0Client.getProfile().nickname}</label>
-            <FontAwesomeIcon icon={faSignOutAlt} onClick={() => {signOut()}} className="signout"/>
+            <label className="nickname">
+              {auth0Client.getProfile().nickname}
+            </label>
+            <FontAwesomeIcon
+              icon={faSignOutAlt}
+              onClick={() => {
+                signOut();
+              }}
+              className="signout"
+            />
           </div>
-        }
+        )}
       </div>
-      <div className="gradient">
-      </div>
+      <div className="gradient"></div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </nav>
-  )
+  );
 }
 
 export default withRouter(NavBar);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const appJsx = (
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain="dev-0r9j-iz3.auth0.com"
+      clientId="1HbLQ6nRcscQnN2UY5i79et3CRBQq3zQ"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </BrowserRouter>
-)
+);
 
-ReactDOM.render(appJsx, document.getElementById('root'))
+ReactDOM.render(appJsx, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import { Auth0Provider } from "@auth0/auth0-react";
+
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain="dev-0r9j-iz3.auth0.com"
+//     clientId="1HbLQ6nRcscQnN2UY5i79et3CRBQq3zQ"
+//     redirectUri={window.location.origin}
+//   >
+//     <App />
+//   </Auth0Provider>,
+//   document.getElementById("root")
+// );
